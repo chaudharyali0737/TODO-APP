8dc31f074adb775ef8b5b7386e4e2d1aeaffdf4f
Ali Akbar <chaudhary.ali.0737@gmail.com>
2023-07-31 12:38:34 +0500

    react benefits nav bar

--- learn-react/src/App.tsx
+++ learn-react/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 function App() {
   return (<>
-    <img src="logo512.png" width="60px" height="60"   alt="logo" />
+    <img src="logo512.png" width="60px" height="60" alt="logo" />
     <nav>
       <h1>MesissaSoft Services</h1>
       <ul>
@@ -16,5 +16,25 @@ function App() {
 
   );
 }
-function React_
-export { App }
+function Reakt_benifits() {
+  return (
+    <>
+      <div>
+        <img src="logo512.png" width="60px" height="60" alt="logo" />
+        <nav>
+          <h1>fun Facts About React</h1>
+          <ul>
+            <li>it was fist used in 2013</li>
+            <li>was originally created by jordan walke</li>
+            <li>has well over 100k stars on github</li>
+            <li>is maintained by facebook</li>
+            <li>empowers thousands of enterprise apps , including mobile apps</li>
+
+          </ul>
+        </nav>
+      </div>
+
+    </>
+  );
+}
+export { App, Reakt_benifits }
--- learn-react/src/index.tsx
+++ learn-react/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {  App } from './App';
+import { App, Reakt_benifits } from './App';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 
@@ -10,7 +10,8 @@ const root = ReactDOM.createRoot(
 root.render(
 
   <React.StrictMode>
-    <App></App>
+    {/* <App></App> */}
+    <Reakt_benifits></Reakt_benifits>
   </React.StrictMode>
 
 );
