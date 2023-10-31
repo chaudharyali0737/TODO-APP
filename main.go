e20b3fe47c0c55a0b060429e774875c1c9e6a8a9
Ali Akbar <chaudhary.ali.0737@gmail.com>
2023-10-31 17:46:44 +0500

    Update main.go

--- main.go
+++ main.go
@@ -4,24 +4,40 @@ import (
 	"api/db"
 	todocrud "api/todoCrud"
 	"log"
+	"net/http"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		c.Next()
+	}
+}
 func main() {
 	db.Init()
 	err := db.TodoMigrations()
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	router := gin.Default()
-	router.Use(cors.Default())
+
+	router.Use(CORSMiddleware())
 	router.GET("/todos", todocrud.GetTodos)
 	router.GET("/todo/:id", todocrud.GetSingleTodos)
 	router.POST("/todo/insert", todocrud.Insert)
 	router.POST("/todo/update", todocrud.Update)
 	router.DELETE("/todo/deleteall", todocrud.DeleteAll)
 	router.DELETE("/todo/delete/:id", todocrud.Delete)
-	router.Run("localhost:8080")
+	router.Run(":8080")
 }
